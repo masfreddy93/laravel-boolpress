92231618472b5fa42300c9dfad2fef64a316a966
masfreddy93 <masieronicola@gmail.com>
2022-10-24 15:39:41 +0200

    edit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
-            PostSeeder::class
+            TagSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
