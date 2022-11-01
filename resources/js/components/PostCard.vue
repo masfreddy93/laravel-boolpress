01659c9732b7b216d6c5e6ab63d011694d93bc39
masfreddy93 <masieronicola@gmail.com>
2022-11-01 11:57:28 +0100

    add date in format d/m/Y

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -8,8 +8,7 @@
         <ul class="flex flex-auto gap-2 mb-3">
             <li v-for="tag in post.tags" :key="tag.id" class="hover:opacity-50 cursor-pointer bg-slate-500 rounded-xl px-2 py-1 text-white">{{ tag.name }}</li>
         </ul>
-        <!-- <p v-if="post.tags">{{ post.tags.name }}</p> -->
-        <span>{{ post.created_at }}</span>
+        <span>{{ post.date }}</span>
     </li>
 </template>
 
