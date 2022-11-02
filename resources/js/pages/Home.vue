62cd95da32789757e31096e4e0d3432a287a8a5b
masfreddy93 <masieronicola@gmail.com>
2022-11-02 16:16:53 +0100

    add vue-router

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div class="container">
+        <h1 class="h1">{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: 'Homepage',
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
